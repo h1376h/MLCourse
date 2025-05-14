ac0c5db7b008764812554706c88f0f9530e06f27
Hamed H. <h1376h@gmail.com>
2025-05-13 22:51:34 -0400

    fix

--- ML_Obsidian_Vault/Codes/ensure_plots_directory.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Create plots directory if it doesn't exist
-if not os.path.exists('plots'):
-    os.makedirs('plots')
-    print("Created plots directory")
-else:
-    print("Plots directory already exists") 
